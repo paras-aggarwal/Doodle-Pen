b25a23e4c0d838654cdd3ca3c2a805f7e733fdd0
Paras Aggarwal <paras.aggarwal@outlook.com>
2019-04-26 02:04:31 +0530

    Removed node_modules from repo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://paras:paras@ds211029.mlab.com:11029/paras_db');
+mongoose.connect(MONGODB_CONNECTION_URI);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
